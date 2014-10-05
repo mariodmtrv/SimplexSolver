d5f81e485229448b0b166274f527d5a3137b1320
Mario Dimitrov <mario.dmtrv@gmail.com>
2014-10-05 20:22:43 +0300

    Finished the kProblem conversion execution

--- FinalProject/Simplexator/src/org/fmi/or/simplexator/algorithm/converter/Problem.java
+++ FinalProject/Simplexator/src/org/fmi/or/simplexator/algorithm/converter/Problem.java
@@ -2,9 +2,11 @@ package org.fmi.or.simplexator.algorithm.converter;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Vector;
 
 import org.fmi.or.simplexator.answerqueue.AnswerQueue;
+import org.fmi.or.simplexator.answerqueue.WebAnswerQueue;
 
 public class Problem extends AbstractProblem {
 
@@ -30,66 +32,49 @@ public class Problem extends AbstractProblem {
 	}
 
 	public void convertToK() {
-		AnswerQueue answerQueue = null;
+		AnswerQueue answerQueue = new WebAnswerQueue(new Locale("bg", "BG"));
 		convertToK(answerQueue);
 	}
 
 	public void convertToK(AnswerQueue answerQueue) {
-		// uiController.addContent(this.toString(), Destination.WINDOW);
-		answerQueue.addMessage("allToMinimum");
-		answerQueue.addProblemStep(this);
-		this.optimum = Optimum.MINIMUM;
 		answerQueue.addProblemStep(this);
 		setToMinimum(answerQueue);
 
-		setRightSidesToPositive();
+		setRightSidesToPositive(answerQueue);
 
-		processNegativeParts();
+		processNegativeParts(answerQueue);
 
-		setToEquations();
+		setToEquations(answerQueue);
 
 	}
 
 	private void setToMinimum(AnswerQueue answerQueue) {
+
+		answerQueue.addMessage("allToMinimum");
 		if (this.optimum != Optimum.MINIMUM) {
-			// uiController.addContent("Ã�Å¸Ã‘â‚¬Ã�ÂµÃ�Â¾Ã�Â±Ã‘â‚¬Ã�Â°Ã�Â·Ã‘Æ’Ã�Â²Ã�Â°Ã�Â¼Ã�Âµ Ã�ÂºÃ‘Å Ã�Â¼ Ã�Â·Ã�Â°Ã�Â´Ã�Â°Ã‘â€¡Ã�Â° Ã�Â·Ã�Â° Ã�Â¼Ã�Â¸Ã�Â½Ã�Â¸Ã�Â¼Ã‘Æ’Ã�Â¼.\n",
-			// Destination.LOG);
-			// uiController.addContent("max(Z)=min(-Z)\n", Destination.LOG);
+			answerQueue.addMessage("toMinimumConversionRule");
 			this.optimum = Optimum.MINIMUM;
 			for (Variable variable : this.zfunction) {
 				variable.changeSign();
 			}
-			// uiController.addContent(this.toString(), Destination.WINDOW);
+			answerQueue.addProblemStep(this);
 			return;
+		} else {
+			answerQueue.addMessage("alreadyMinimum");
 		}
-		// uiController.addContent("Ã�Å¾Ã‘â‚¬Ã�Â¸Ã�Â³Ã�Â¸Ã�Â½Ã�Â°Ã�Â»Ã�Â½Ã�Â°Ã‘â€šÃ�Â° Ã�Â·Ã�Â°Ã�Â´Ã�Â°Ã‘â€¡Ã�Â° Ã�Âµ Ã�Â·Ã�Â° Ã�Â¼Ã�Â¸Ã�Â½Ã�Â¸Ã�Â¼Ã‘Æ’Ã�Â¼.",
-		// Destination.LOG);
+
 	}
 
-	private void setRightSidesToPositive() {
-		// uiController
-		// .addContent(
-		// "Ã�Â¢Ã‘â‚¬Ã�Â°Ã�Â½Ã‘ï¿½Ã‘â€žÃ�Â¾Ã‘â‚¬Ã�Â¼Ã�Â¸Ã‘â‚¬Ã�Â°Ã�Â¼Ã�Âµ Ã�Â²Ã‘ï¿½Ã�Â¸Ã‘â€¡Ã�ÂºÃ�Â¸ Ã�Â¾Ã�Â³Ã‘â‚¬Ã�Â°Ã�Â½Ã�Â¸Ã‘â€¡Ã�ÂµÃ�Â½Ã�Â¸Ã‘ï¿½ Ã‘ï¿½ Ã�Â¾Ã‘â€šÃ‘â‚¬Ã�Â¸Ã‘â€ Ã�Â°Ã‘â€šÃ�ÂµÃ�Â»Ã�Â½Ã�Â¸ Ã�Â´Ã�ÂµÃ‘ï¿½Ã�Â½Ã�Â¸ Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â¸ Ã�Â² Ã‘â€šÃ�Â°Ã�ÂºÃ�Â¸Ã�Â²Ã�Â° Ã‘ï¿½ Ã�Â¿Ã�Â¾Ã�Â»Ã�Â¾Ã�Â¶Ã�Â¸Ã‘â€šÃ�ÂµÃ�Â»Ã�Â½Ã�Â¸.",
-		// Destination.LOG);
+	private void setRightSidesToPositive(AnswerQueue answerQueue) {
+		answerQueue.addMessage("rightSidesToPositiveRule");
 		for (Restriction restriction : restrictions) {
 			restriction.rightSideToPositive();
 		}
-		// uiController.addContent(this.toString(), Destination.WINDOW);
+		answerQueue.addProblemStep(this);
 	}
 
-	private void setToEquations() {
-		// uiController
-		// .addContent(
-		// "Ã�â€”Ã�Â° Ã�Â²Ã‘ï¿½Ã‘ï¿½Ã�ÂºÃ�Â¾ Ã�Â½Ã�ÂµÃ‘â‚¬Ã�Â°Ã�Â²Ã�ÂµÃ�Â½Ã‘ï¿½Ã‘â€šÃ�Â²Ã�Â¾ Ã�Â² Ã�Â¾Ã�Â³Ã‘â‚¬Ã�Â°Ã�Â½Ã�Â¸Ã‘â€¡Ã�ÂµÃ�Â½Ã�Â¸Ã‘ï¿½Ã‘â€šÃ�Â° Ã�Â·Ã�Â°Ã�Â¼Ã�ÂµÃ‘ï¿½Ã‘â€šÃ�Â²Ã�Â°Ã�Â¼Ã�Âµ Ã‘ï¿½ Ã‘â‚¬Ã�Â°Ã�Â²Ã�ÂµÃ�Â½Ã‘ï¿½Ã‘â€šÃ�Â²Ã�Â¾, Ã�ÂºÃ�Â°Ã‘â€šÃ�Â¾ Ã�Â´Ã�Â¾Ã�Â±Ã�Â°Ã�Â²Ã‘ï¿½Ã�Â¼Ã�Âµ Ã�Â´Ã�Â¾Ã�Â¿Ã‘Å Ã�Â»Ã�Â½Ã�Â¸Ã‘â€šÃ�ÂµÃ�Â»Ã�Â½Ã�Â° Ã�Â¿Ã‘â‚¬Ã�Â¾Ã�Â¼Ã�ÂµÃ�Â½Ã�Â»Ã�Â¸Ã�Â²Ã�Â°.",
-		// Destination.LOG);
-		// uiController
-		// .addContent(
-		// "<Ã�Â»Ã‘ï¿½Ã�Â²Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°> >= <Ã�Â´Ã‘ï¿½Ã‘ï¿½Ã�Â½Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°>		Ã‘ï¿½Ã‘â€šÃ�Â°Ã�Â²Ã�Â°		<Ã�Â»Ã‘ï¿½Ã�Â²Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°> - \\x_k = <Ã�Â´Ã‘ï¿½Ã‘ï¿½Ã�Â½Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°>, Ã�ÂºÃ‘Å Ã�Â´Ã�ÂµÃ‘â€šÃ�Â¾ k Ã�Âµ Ã�Â½Ã�Â¾Ã�Â² Ã�Â¸Ã�Â½Ã�Â´Ã�ÂµÃ�ÂºÃ‘ï¿½ Ã�Â¸ \\x_k >= 0.",
-		// Destination.LOG);
-		// uiController
-		// .addContent(
-		// "<Ã�Â»Ã‘ï¿½Ã�Â²Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°> <= <Ã�Â´Ã‘ï¿½Ã‘ï¿½Ã�Â½Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°>		Ã‘ï¿½Ã‘â€šÃ�Â°Ã�Â²Ã�Â°		<Ã�Â»Ã‘ï¿½Ã�Â²Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°> + \\x_k = <Ã�Â´Ã‘ï¿½Ã‘ï¿½Ã�Â½Ã�Â°_Ã‘ï¿½Ã‘â€šÃ‘â‚¬Ã�Â°Ã�Â½Ã�Â°>, Ã�ÂºÃ‘Å Ã�Â´Ã�ÂµÃ‘â€šÃ�Â¾ k Ã�Âµ Ã�Â½Ã�Â¾Ã�Â² Ã�Â¸Ã�Â½Ã�Â´Ã�ÂµÃ�ÂºÃ‘ï¿½ Ã�Â¸ \\x_k >= 0.",
-		// Destination.LOG);
+	private void setToEquations(AnswerQueue answerQueue) {
+		answerQueue.addMessage("toEquationsRule");
 		for (Restriction restriction : restrictions) {
 			Variable newVar = restriction.setToEquation(maxIndex);
 			if (newVar != null) {
@@ -100,6 +85,7 @@ public class Problem extends AbstractProblem {
 				// we add with zero to keep table shape consistency
 				for (int i = 0; i < restrictions.size(); i++) {
 					if (restrictions.get(i).getVarCount() < zfunction.size()) {
+						answerQueue.addProblemStep(this);
 						addVarToRestriction(i, toAdd);
 					} else {
 						// we are at the restriction already set to equation
@@ -115,14 +101,12 @@ public class Problem extends AbstractProblem {
 		restrictions.set(restrIndex, updated);
 	}
 
-	private void processNegativeParts() {
+	private void processNegativeParts(AnswerQueue answerQueue) {
 
-		// uiController
-		// .addContent(
-		// "Ã�â€”Ã�Â° Ã�Â²Ã‘ï¿½Ã‘ï¿½Ã�ÂºÃ�Â° Ã�Â¿Ã‘â‚¬Ã�Â¾Ã�Â¼Ã�ÂµÃ�Â½Ã�Â»Ã�Â¸Ã�Â²Ã�Â° \\x_i, Ã�Â·Ã�Â° Ã�ÂºÃ�Â¾Ã‘ï¿½Ã‘â€šÃ�Â¾ Ã�Â½Ã‘ï¿½Ã�Â¼Ã�Â° Ã�Â¾Ã�Â³Ã‘â‚¬Ã�Â°Ã�Â½Ã�Â¸Ã‘â€¡Ã�ÂµÃ�Â½Ã�Â¸Ã�Âµ Ã�Â·Ã�Â° Ã�Â½Ã�ÂµÃ�Â¾Ã‘â€šÃ‘â‚¬Ã�Â¸Ã‘â€ Ã�Â°Ã‘â€šÃ�ÂµÃ�Â»Ã�Â½Ã�Â¾Ã‘ï¿½Ã‘â€š, Ã‘ï¿½ Ã�Â·Ã�Â°Ã�Â¼Ã�ÂµÃ‘ï¿½Ã‘â€šÃ�Â²Ã�Â°Ã�Â¼Ã�Âµ Ã‘ï¿½ 2 Ã�Â¿Ã‘â‚¬Ã�Â¾Ã�Â¼Ã�ÂµÃ�Â½Ã�Â»Ã�Â¸Ã�Â²Ã�Â¸ \\x_i^+ Ã�Â¸ \\x_i^-, Ã�ÂºÃ�Â¾Ã�Â¸Ã‘â€šÃ�Â¾ Ã‘ï¿½Ã�Â° Ã�Â½Ã�ÂµÃ�Â¾Ã‘â€šÃ‘â‚¬Ã�Â¸Ã‘â€ Ã�Â°Ã‘â€šÃ�ÂµÃ�Â»Ã�Â½Ã�Â¸.",
-		// Destination.LOG);
+		answerQueue.addMessage("processNegativeParts");
 		int varsPassed = 0;
 		Iterator<Variable> zfuncIter = zfunction.iterator();
+		Boolean processedOne = false;
 		for (Boolean hnp : hasNegativePart) {
 			Variable variable = (Variable) zfuncIter.next();
 			varsPassed++;
@@ -146,10 +130,15 @@ public class Problem extends AbstractProblem {
 					changedRestriction.bipartizeVariable(varIndex);
 					restrictions.set(restrictionIndex, changedRestriction);
 				}
+				processedOne = true;
+				answerQueue.addProblemStep(this);
 			}
 
 		}
-		// uiController.addContent(this.toString(), Destination.WINDOW);
+		if (processedOne == false) {
+			answerQueue.addMessage("noNegativesAtRightSide");
+			answerQueue.addProblemStep(this);
+		}
 	}
 
 	public String toString() {
