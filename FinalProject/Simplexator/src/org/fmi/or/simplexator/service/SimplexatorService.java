1034b966fd4106db36a49f3ce1719bb6ee381db2
Mario Dimitrov <mario.dmtrv@gmail.com>
2014-10-23 00:04:11 +0300

    Updated

--- FinalProject/Simplexator/src/org/fmi/or/simplexator/service/SimplexatorService.java
+++ FinalProject/Simplexator/src/org/fmi/or/simplexator/service/SimplexatorService.java
@@ -29,65 +29,10 @@ public class SimplexatorService {
 	@Consumes("application/json")
 	@Produces("application/json")
 	public SolutionResponse test(InputProblem inputProblem) {
-		SolutionResponse response = new SolutionResponse(createProblem());
+		SolutionResponse response = new SolutionResponse(
+				inputProblem.getProblem());
 		response.solve();
 		return response;
-	}
-
-	/*
-	 * public ProblemConversionQueue getSteps() { ProblemConversionQueue queue =
-	 * new ProblemConversionQueue();
-	 * 
-	 * // problem.convertToK(queue);
-	 * 
-	 * /* List<TransformationStep> steps = new ArrayList<>(); steps.add(new
-	 * TransformationStep(queue.getProblemSteps().get(0))); steps.add(new
-	 * TransformationStep(queue.getProblemSteps().get(1))); return queue; }
-	 */
-	@GET
-	@Produces("application/json")
-	@Path("/get-step")
-	public TransformationStep getStep() {
-		List<Variable> zfunction = new LinkedList<Variable>();
-		zfunction.add(new Variable(new Fraction(2), 1));
-		zfunction.add(new Variable(new Fraction(1), 2));
-		zfunction.add(new Variable(new Fraction(2), 3));
-
-		Vector<Restriction> restrictions = new Vector<Restriction>();
-		List<Variable> firstRestr = new LinkedList<Variable>();
-		firstRestr.add(new Variable(new Fraction(1), 1));
-		firstRestr.add(new Variable(new Fraction(0), 2));
-		firstRestr.add(new Variable(new Fraction(-1), 3));
-		Restriction first = new Restriction(firstRestr, EquationSign.LTE,
-				new Fraction(-1));
-
-		List<Variable> secondRestr = new LinkedList<Variable>();
-		secondRestr.add(new Variable(new Fraction(-1), 1));
-		secondRestr.add(new Variable(new Fraction(0), 2));
-		secondRestr.add(new Variable(new Fraction(-2), 3));
-		Restriction second = new Restriction(secondRestr, EquationSign.LTE,
-				new Fraction(3));
-
-		List<Variable> thirdRestr = new LinkedList<Variable>();
-		thirdRestr.add(new Variable(new Fraction(3), 1));
-		thirdRestr.add(new Variable(new Fraction(1), 2));
-		thirdRestr.add(new Variable(new Fraction(1), 3));
-		Restriction third = new Restriction(thirdRestr, EquationSign.EQ,
-				new Fraction(4));
-		restrictions.add(first);
-		restrictions.add(second);
-		restrictions.add(third);
-
-		Optimum optimum = Optimum.MINIMUM;
-		Vector<Boolean> hasNegativePart = new Vector<>();
-		hasNegativePart.add(true);
-		hasNegativePart.add(false);
-		hasNegativePart.add(false);
-		Problem problem = new Problem(zfunction, restrictions, optimum,
-				hasNegativePart);
-		problem.convertToK();
-		TransformationStep step = new TransformationStep(problem);
-		return step;
 	}
 
 	private Problem createProblem() {
@@ -129,20 +74,6 @@ public class SimplexatorService {
 		Problem problem = new Problem(zfunction, restrictions, optimum,
 				hasNegativePart);
 		return problem;
-	}
-
-	/*
-	 * @POST
-	 * 
-	 * @Consumes("application/json") public Response createProduct(Product
-	 * product) { String result = "Product created : " + product; return
-	 * Response.status(201).entity(result).build(); }
-	 */
-	@GET
-	@Path("/latex")
-	@Produces("text/plain")
-	public String getLatexContent() {
-		return "Latex content";
 	}
 
 }
